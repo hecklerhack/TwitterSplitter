83366a82911ff3b06f071b3ae4969ceed1fd6533
Trish Moreno <trishmoreno02@gmail.com>
2018-05-08 18:30:32 +0800

    simple UI added in html; encapsulated algorithm into splitMessage() in index.js

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-var msg = document.getElementById("msg").textContent;
-var lines = [""];
-var counter = 0;
-var index = 0;
-for(var i = 0; i < msg.length; i++)
+document.getElementById("enter").addEventListener('click', splitMessage);
+
+function splitMessage(e)
 {
-  counter = counter + 1;
-  var str1 = lines[index];
-  if(counter + 4 < 50)
+  var msg = document.getElementById("tweet").value;
+  var lines = [""];
+  var counter = 0;
+  var index = 0;
+  for(var i = 0; i < msg.length; i++)
   {
-    lines[index] = str1.concat(msg.charAt(i));
+    counter = counter + 1;
+    var str1 = lines[index];
+    if(counter + 4 < 50)
+    {
+      lines[index] = str1.concat(msg.charAt(i));
+    }
+    else {
+      index++;
+      str1 = "";
+      lines[index] = str1.concat(msg.charAt(i));
+      counter = 0;
+    }
   }
-  else {
-    index++;
-    str1 = "";
-    lines[index] = str1.concat(msg.charAt(i));
-    counter = 0;
+
+  var board = document.getElementById("tweetHolder");
+
+  for(var j = 0; j < lines.length; j++)
+  {
+    var str2 = (j+1) + "/" + lines.length + " ";
+    lines[j] = str2.concat(lines[j]);
+    board.insertAdjacentHTML("beforeend", lines[j] + "<br>");
   }
+//  board.insertAdjacentHTML("beforeend", "</div></div>");
 }
-
-for(var j = 0; j < lines.length; j++)
-{
-  var str2 = (j+1) + "/" + lines.length + " ";
-  lines[j] = str2.concat(lines[j]);
-}
-
-console.log(lines);
